34b4b9f03b96992162af9faf249e8e478c664106
AaEzha <aaezha@gmail.com>
2021-06-29 11:13:46 +0700

    First init

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+	public function index()
+	{
+		$data['title'] = "Dashboard";
+		$data['konten'] = "dashboard";
+		$this->load->view('template/header', $data);
+	}
+}
---
+++ application/views/template/menu.php
@@ -0,0 +1,31 @@
+			<!-- Heading -->
+			<div class="sidebar-heading">
+				Main Menu
+			</div>
+
+			<!-- Nav Item - Pages Collapse Menu -->
+			<!-- <li class="nav-item active">
+				<a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+					<i class="fas fa-fw fa-folder"></i>
+					<span>Pages</span>
+				</a>
+				<div id="collapsePages" class="collapse show" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+					<div class="bg-white py-2 collapse-inner rounded">
+						<h6 class="collapse-header">Login Screens:</h6>
+						<a class="collapse-item" href="login.html">Login</a>
+						<a class="collapse-item" href="register.html">Register</a>
+						<a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+						<div class="collapse-divider"></div>
+						<h6 class="collapse-header">Other Pages:</h6>
+						<a class="collapse-item" href="404.html">404 Page</a>
+						<a class="collapse-item active" href="blank.html">Blank Page</a>
+					</div>
+				</div>
+			</li> -->
+
+			<li class="nav-item">
+				<a class="nav-link" href="<?=base_url();?>">
+					<i class="fas fa-fw fa-list"></i>
+					<span>Some Menu</span>
+				</a>
+			</li>
